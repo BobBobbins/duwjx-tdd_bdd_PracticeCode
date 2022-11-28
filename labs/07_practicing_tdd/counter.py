da19969d90a7ea0a3b4fbefff0226d4eea6fa782
Robert Bobbins <11790124+BobBobbins@users.noreply.github.com>
2022-11-28 00:38:20 -0500

    Test driven dev routes.

--- labs/07_practicing_tdd/counter.py
+++ labs/07_practicing_tdd/counter.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 COUNTERS = {}
 
+
 @app.route("/counters/<name>", methods=["POST"])
 def create_counter(name):
     """Creates a counter"""
@@ -12,7 +13,45 @@ def create_counter(name):
     global COUNTERS
 
     if name in COUNTERS:
-        return {"message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+        return {"message": f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
 
     COUNTERS[name] = 0
-    return { name: COUNTERS[name] }, status.HTTP_201_CREATED
+    return {name: COUNTERS[name]}, status.HTTP_201_CREATED
+
+
+@app.route("/counters/<name>", methods=["PUT"])
+def increment_counter(name):
+    """Increments a counter"""
+    app.logger.info(f"Request to increment counter: {name}")
+    global COUNTERS
+
+    if name in COUNTERS:
+        COUNTERS[name] += 1
+        return {name: COUNTERS[name]}, status.HTTP_200_OK
+
+    return {"message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+
+
+@app.route("/counters/<name>", methods=["GET"])
+def read_counter(name):
+    """Reads a counter"""
+    app.logger.info(f"Request to read counter: {name}")
+    global COUNTERS
+
+    if name in COUNTERS:
+        return {name: COUNTERS[name]}, status.HTTP_200_OK
+
+    return {"message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
+
+
+@app.route("/counters/<name>", methods=["DELETE"])
+def delete_counter(name):
+    """Reads a counter"""
+    app.logger.info(f"Request to delete counter: {name}")
+    global COUNTERS
+
+    if name in COUNTERS:
+        del COUNTERS[name]
+        return {}, status.HTTP_204_NO_CONTENT
+
+    return {"message": f"Counter {name} does not exist"}, status.HTTP_404_NOT_FOUND
--- labs/07_practicing_tdd/test_counter.py
+++ labs/07_practicing_tdd/test_counter.py
@@ -5,6 +5,7 @@ from unittest import TestCase
 import status
 from counter import app
 
+
 class CounterTest(TestCase):
     """Test Cases for Counter Web Service"""
 
@@ -13,15 +14,46 @@ class CounterTest(TestCase):
 
     def test_create_a_counter(self):
         """It should create a counter"""
-        result = self.client.post("/counters/foo")
-        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
-        data = result.get_json()
+        response = self.client.post("/counters/foo")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        data = response.get_json()
         self.assertIn("foo", data)
         self.assertEqual(data["foo"], 0)
 
     def test_duplicate_counter(self):
         """It should return an error for duplicates"""
-        result = self.client.post("/counters/bar")
-        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
-        result = self.client.post("/counters/bar")
-        self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+        response = self.client.post("/counters/bar")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.post("/counters/bar")
+        self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
+
+    def test_update_a_counter(self):
+        """Test updating a counter"""
+        response = self.client.post("/counters/baz")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        initial_count = response.get_json()["baz"]
+        # Update the counter
+        response = self.client.put("/counters/baz")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        count = response.get_json()["baz"]
+        self.assertEqual(count, initial_count + 1)
+
+    def test_read_a_counter(self):
+        """It should read the counter"""
+        response = self.client.post("/counters/bin")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # Read the counter
+        response = self.client.get("/counters/bin")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        count = response.get_json()["bin"]
+        self.assertEqual(count, 0)
+
+    def test_delete_a_counter(self):
+        """It should delete the counter"""
+        response = self.client.post("/counters/fob")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # Delete the counter
+        response = self.client.delete("/counters/fob")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        response = self.client.delete("/counters/fob")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
